2c59151c0274602106d77a70e461c29cd5853b3d
xovaaan <xovan089@gmail.com>
2023-11-28 00:34:15 +0600

    sns

--- react 22/src/Components/Contact_Form/Form.jsx
+++ react 22/src/Components/Contact_Form/Form.jsx
@@ -11,6 +11,21 @@ const form = () => {
       <div className={styles.email}>
         <button>Via Email Form</button>
       </div>
+      <div className={styles.input}>
+        <div className={styles.inputs}>
+        <label htmlFor="name">Name</label>
+        <input type="text" name="Name" id="texts" placeholder="Your Name" />
+        </div>
+        <div className={styles.inputs}>
+        <label htmlFor="name">E-mail</label>
+        <input type="text" name="Name" id="texts" placeholder="Your E-mail" />
+        </div>
+        <div className={styles.texting}>
+        <label htmlFor="name">Text</label>
+        <input  type="text" name="Name" id="texts" />
+        </div>
+       
+      </div>
      </div>
       <div className="images">
 
